092eb1c845a879e7e0240d62a07356668edadc07
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2022-10-26 01:18:38 -0500

    Added: Create entry endpoint

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -11,6 +11,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
   switch (req.method) {
     case 'GET':
       return getEntries(res)
+
+    case 'POST':
+      return postEntry(req, res)
   
     
     default:
@@ -25,5 +28,32 @@ const getEntries = async(res: NextApiResponse<Data>) => {
   const entries = await Entry.find().sort({ createdAt: 'asc' })
   await db.disconnect()
 
-  res.status(200).json(entries)
+  return res.status(200).json(entries)
+}
+
+const postEntry = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+  const { description = '' } =  req.body
+
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now()
+  })
+
+
+  try {
+    
+    await db.connect()
+    await newEntry.save()
+    await db.disconnect()
+
+    return res.status(201).json(newEntry)
+
+  } catch (error) {
+    await db.disconnect()
+    console.log(error)
+
+    return res.status(500).json({ message: 'Algo salio mal, revisar consola del servidor' })
+  }
+
 }
